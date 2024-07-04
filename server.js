646932f8dc810c1fa078198f3e5717c771f31e06
André <andrelimasa21@gmail.com>
2024-07-04 19:39:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -148,26 +148,43 @@ app.post("/login", async (req, res) => {
 
   try {
     const user = await User.findOne({ email });
+
     if (!user) {
-      console.log("User not found");
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
+
     if (!isPasswordValid) {
-      console.log("Invalid password");
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
+
     res.json({
       accessToken: token,
       redirectUrl: "https://locals-v1.onrender.com",
     });
   } catch (error) {
     console.error("Login error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Registration error:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
